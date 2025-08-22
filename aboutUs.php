9af31e2e8663e4f04c42476ce2c35410297d6119
Chenchen <81400843+Grasskekw@users.noreply.github.com>
2025-08-22 21:35:04 +0800

    UPDATE ABOUTUS.PHP
    USING NEW STYLE, WORK IN PROGRESS

--- aboutUs.php
+++ aboutUs.php
@@ -12,7 +12,17 @@ $userName = $isLoggedIn ? $_SESSION['user_name'] : "Guest";
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>About Us - VetGroom Hub</title>
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
   <style>
     /* Profile dropdown */
     .profile-dropdown {
