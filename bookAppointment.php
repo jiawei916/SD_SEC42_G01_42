b29afce3f10495a86aae072d205e23636cce1a36
LXianWei <limxianwei123@gmail.com>
2025-09-22 00:41:09 +0800

    UPDATE BOOK APPOINTMENT UI

--- bookAppointment.php
+++ bookAppointment.php
@@ -220,7 +220,7 @@ $conn->close();
             padding: 40px;
             border-radius: 12px;
             box-shadow: 0 4px 20px rgba(0,0,0,0.1);
-            margin-top: 50px;
+            margin-top: 100px;
             margin-bottom: 50px;
         }
         
